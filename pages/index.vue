16ff0e50d0ebaffaace23c9c4604ff6f9d9a1358
Sanjay Dutta <sanjay.aug.02.1998@gmail.com>
2022-08-15 15:38:39 +0530

    Added pages/api

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h4>02/08/1998</h4>
+    <h1>{{ message }}</h1>
+    <h2>Enter your Name: <input type="text" v-model=name></h2>
+    <button @click="changeName">Click to change name</button>
+      <h1 v-if="buttonClicked">{{ inputName }}</h1>
+    <button @click="callApi">Get Weather</button><br>
+    {{weatherInfo}}
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      message: 'Testing Azure+Github Deployment',
+      name:"",
+      inputName:"",
+      buttonClicked:true,
+      weatherInfo:null
+    }
+  },
+  methods: {
+    changeName() {
+      this.inputName = `Hello ${this.name}`
+     //this.buttonClicked = !this.buttonClicked
+    },
+    callApi(){
+      console.log('Calling API')
+      axios.get('/api/test').then(response=>{
+        console.log(response.data)
+        this.weatherInfo = response.data
+      }).catch
+    }
+  }
 }
 </script>
